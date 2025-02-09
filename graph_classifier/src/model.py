00b3bc062e6c0f9563f4381f60d6a0a705b77885
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2025-02-09 10:17:29 +0000

    fix: model non sigmoid

--- graph_classifier/src/model.py
+++ graph_classifier/src/model.py
@@ -10,11 +10,7 @@ class ResNet50BinaryClassifier(nn.Module):
         # 最終全結合層を置き換え（二値分類のため）
         num_features = self.resnet.fc.in_features
         self.resnet.fc = nn.Linear(num_features, 1)  
-        
-        # シグモイド関数を用いて確率値を出力
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.resnet(x)
-        x = self.sigmoid(x) 
         return x
